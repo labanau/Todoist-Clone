e36e60113f7dce4fd488582f5b3e4e6220708920
Rokas Labanauskas <lrokas8@gmail.com>
2019-09-22 22:49:33 +0100

    Added firebaseConfig and seperated the keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 /* eslint-disable linebreak-style */
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import apiConfig from '../src/apiKeys';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectdId: '',
-  storageBucket: '',
-  messageSenderId: '',
-  apiId: '',
+  apiKey: apiConfig.apiKey,
+  authDomain: apiConfig.authDomain,
+  databaseURL: apiConfig.databaseURL,
+  projectdId: apiConfig.projectdId,
+  storageBucket: apiConfig.storageBucket,
+  messageSenderId: apiConfig.messageSenderId,
+  apiId: apiConfig.apiId,
 });
 
 export {firebaseConfig as firebase};
